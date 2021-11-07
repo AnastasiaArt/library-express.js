f866883282970372a9dc16145019c51fcbceae4d
art <art-nastya@inbox.ru>
2021-11-07 13:44:31 +0300

    added mongodb , mongoose

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const error404 = require('./middleware/404Error');
 const userApiRouter = require('./routes/api/userRouter');
 const booksApiRouter = require('./routes/api/booksRouter');
 const indexRouter = require('./routes/index');
-const booksRouter = require('./routes/books')
+const booksRouter = require('./routes/books');
+const mongoose = require('mongoose');
 
 // создаем объект приложения
 const app = express();
@@ -28,7 +29,33 @@ app.use(returnError)
 app.use(error404)
 
 const PORT = process.env.PORT || 3000;
+const UserDB = process.env.DB_USERNAME;
+const PasswordDB = process.env.DB_PASSWORD;
+const NameDB = process.env.DB_NAME
+const HostDb = process.env.DB_HOST
 
-app.listen(PORT, () => {
-    console.log(`=== Library start server on PORT ${PORT} ===`);
-});
+// подключение mongoose
+async function start() {
+    try {
+        //const UrlDB = `mongodb+srv://${UserDB}:${PasswordDB}@cluster0.grfrs.mongodb.net/${NameDB}`;
+        //const UrlDB = `mongodb://localhost:27017/mydb`;
+        //const UrlDB = `mongodb://${UserDB}:${PasswordDB}@localhost:27017/mydb`;
+        //await mongoose.connect(UrlDb);
+
+        await mongoose.connect(HostDb, {
+            user: UserDB,
+            pass: PasswordDB,
+            dbName: NameDB,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
