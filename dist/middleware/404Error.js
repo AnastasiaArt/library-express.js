f92d0681f841232b26e704af041039c4fb9ba18e
art <art-nastya@inbox.ru>
2021-12-23 18:48:55 +0300

    transport to ts

--- dist/middleware/404Error.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (req, res) => {
-    res.render("error/404", {
-        title: "404 | страница не найдена",
-    });
-};
